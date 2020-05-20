c7caebeaa424025d5c0a5624cdb550993c00b985
Wilman Orellano Heredia <57116131+wjorellano@users.noreply.github.com>
2020-05-19 23:53:58 -0500

    pruebas unitarias

---
+++ calculator.js
@@ -0,0 +1,7 @@
+exports.addTest = function(valor1, valor2){
+    let resultado = valor1 + valor2;
+    return resultado;
+}
+
+//  este progama se ejecuta desde la terminal con el comando "npm test" y tiene 2 tipos de pruebas
+//  con Mocha y Chai js
